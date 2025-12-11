096a1328023efa763cd246d6c1315ad497a12cf9
Sophie Archibald <arc21004@byui.edu>
2025-12-11 14:56:25 -0700

    Loaded desserts into local storage

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,18 @@
 import { generateRandomIndex } from "./utilities.mjs";
 
-// export async function loadDesserts() {
-//   const response = await fetch('https://www.themealdb.com/api/json/v1/1/filter.php?c=Dessert');
-//   let data = await response.json();
-//   console.log(data)
+export async function loadDesserts() {
+  const response = await fetch('https://www.themealdb.com/api/json/v1/1/filter.php?c=Dessert');
+  let data = await response.json();
+  console.log(data)
 
-//   localStorage.setItem("desserts", JSON.stringify(data.meals));
-// }
+  localStorage.setItem("desserts", JSON.stringify(data.meals));
+}
 
-// loadDesserts();
+loadDesserts();
 
-let desserts = JSON.parse(localStorage.getItem("desserts"));
+let desserts = await JSON.parse(localStorage.getItem("desserts"));
 
-console.log(desserts[0]);
+// console.log(await desserts[0]);
 
 console.log(generateRandomIndex);
 
